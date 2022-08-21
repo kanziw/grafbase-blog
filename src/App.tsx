d080d816b4c0d02720a0693d053ca76a91ac6ab2
Jiwoong Jung <kanziwoong@gmail.com>
2022-08-21 15:45:22 +0900

    Add schema

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom'
 
 const games = [
   {
+    id: 'space-worm',
     name: 'SpaceWorm',
     path: '/space-worm',
     imgSrc: '/images/space-worm.jpg',
@@ -15,8 +16,8 @@ const App = () => (
     <h1 className="title">미니앱게임천국👼</h1>
     <section className="games">
       <ul>
-        {games.map((game, idx) => (
-          <li key={`${game.name}-${idx}`} className="game">
+        {games.map((game) => (
+          <li key={`${game.id}`} className="game">
             <Link to={game.path}>
               <div className="game-logo-img"><img src={game.imgSrc} alt={game.name} /></div>
               <div className="game-name">{game.name}</div>
