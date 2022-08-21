fc96f32e52b5527ca476c0acd2a27a20601395fa
Jiwoong Jung <kanziwoong@gmail.com>
2022-08-21 14:49:57 +0900

    Add games page and logo

--- README.md
+++ README.md
@@ -6,3 +6,4 @@
 
 * Game: https://github.com/dancramp/js13k-2021
 * Favicon: https://favicon.io/emoji-favicons/worm/
+* Logo: DALL·E https://labs.openai.com/
--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,30 @@
+import './App.css'
+
 import { Link } from 'react-router-dom'
+
+const games = [
+  {
+    name: 'SpaceWorm',
+    path: '/space-worm',
+    imgSrc: '/images/space-worm.jpg',
+  },
+]
 
 const App = () => (
   <>
-    Games
-    <ul>
-      <li>- <Link to="/space-worm">SpaceWorm</Link></li>
-    </ul>
+    <h1 className="title">미니앱게임천국👼</h1>
+    <section className="games">
+      <ul>
+        {games.map((game, idx) => (
+          <li key={`${game.name}-${idx}`} className="game">
+            <Link to={game.path}>
+              <div className="game-logo-img"><img src={game.imgSrc} alt={game.name} /></div>
+              <div className="game-name">{game.name}</div>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </section>
   </>
 )
 
