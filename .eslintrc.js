996dd7a2e8bea10a63628a255aa47c6b0665473f
Jiwoong Jung <kanziwoong@gmail.com>
2022-08-21 12:57:14 +0900

    Add react eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,24 @@
 module.exports = {
   extends: [
     'kanziw',
+    'plugin:react/recommended',
+    'react-app',
   ],
   parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
     tsconfigRootDir: __dirname,
     project: ['./tsconfig.eslint.json'],
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  rules: {
+    'react/jsx-indent': [2, 2],
+    'react/jsx-indent-props': [2, 2],
+    'react/react-in-jsx-scope': 'off',
   },
 }
