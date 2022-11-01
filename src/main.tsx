04383b4431886d9075e1ad36cbf93fcfb478d5dd
kanziw <kanziwoong@gmail.com>
2022-11-01 15:00:40 +0900

    Use HashRouter

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import './firebase'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import {
-  BrowserRouter as Router,
+  HashRouter as Router,
   Route,
   Routes,
 } from 'react-router-dom'
