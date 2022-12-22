6c269ea488b6ba7563be57507d3c49497e2a0f2b
Jiwoong Jung <kanziwoong@gmail.com>
2022-12-23 01:22:40 +0900

    Update FillText

--- src/pages/space-worm/gameOnCanvas.ts
+++ src/pages/space-worm/gameOnCanvas.ts
@@ -1019,21 +1019,26 @@ export const gameOnCanvas = (spaceWorm_canvas, { top10Scores, ...callbacks }: Pr
 
     // different screens based on state
     if (spaceWorm_state === 'start') {
+      const startY = -16;
+      const fillText = (text: string, height: number) => {
+        spaceWorm_ctx.fillText(text, 0, height + startY)
+      }
+
       spaceWorm_ctx.font = '4px Arial'
       spaceWorm_ctx.fillStyle = 'white'
       spaceWorm_ctx.textAlign = 'center'
-      spaceWorm_ctx.fillText('SPACE WORM!', 0, 0)
+      fillText('SPACE WORM!', -4)
 
       const rankCellHeight = 1.8
       spaceWorm_ctx.font = `${rankCellHeight * 0.8}px Arial`
-      spaceWorm_ctx.fillText('---------------------- TOP 10 ----------------------', 0, 3)
+      fillText('---------------------- TOP 10 ----------------------', 3)
       const rankHeight = 3 + rankCellHeight 
       top10Scores.forEach((score, idx) => {
-        spaceWorm_ctx.fillText(`[${score.rank}]\tscore: ${score.score},\tname: ${score.user.displayName}`, 0, rankHeight + (idx * rankCellHeight))
+        fillText(`[${score.rank}]\tscore: ${score.score},\tname: ${score.user.displayName}`, rankHeight + (idx * rankCellHeight))
       })
-      spaceWorm_ctx.fillText('--------------------------------------------------------', 0, rankHeight + (top10Scores.length * rankCellHeight))
+      fillText('--------------------------------------------------------', rankHeight + (top10Scores.length * rankCellHeight))
       spaceWorm_ctx.font = '2px Arial'
-      spaceWorm_ctx.fillText('Click/tap to continue.', 0, rankHeight + (top10Scores.length * rankCellHeight) + 2)
+      fillText('Click/tap to continue.', 0, rankHeight + (top10Scores.length * rankCellHeight) + 2)
     } else if (spaceWorm_state === 'levelstart') {
       spaceWorm_ctx.font = '4px Arial'
       spaceWorm_ctx.fillStyle = 'white'
